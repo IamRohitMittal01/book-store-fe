b87efb26c5e575cb514007599540423c810ccb7f
Rohit Mittal <rohit.mittal.maltem@sgx.com>
2023-08-06 19:06:16 +0800

    Changes for refresh

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './booklist.css';
 
@@ -13,23 +13,56 @@ interface Book {
 
 const BookList: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const fetchBooks = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get<Book[]>('http://localhost:3000/books', {
+        params: { page },
+      });
+      setBooks((prevBooks) => [...prevBooks, ...response.data]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.error('Error fetching books:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    // Make an API call to fetch all books from the server
-    axios
-      .get<Book[]>('http://localhost:3000/books')
-      .then((response) => {
-        setBooks(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching books:', error);
-      });
+    fetchBooks();
   }, []);
+
+  const handleIntersection: IntersectionObserverCallback = (entries) => {
+    const target = entries[0];
+    if (target.isIntersecting && !loading) {
+      fetchBooks();
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: '50px', // Trigger the callback 50px before the bottom of the container
+      threshold: 0,
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [loading]);
 
   return (
     <div>
       <h1>Book List</h1>
-      <div className="book-list">
+      <div className="book-list" ref={containerRef}>
         {books.map((book) => (
           <div className="book-tile" key={book.id}>
             <img src={book.coverImage} alt={book.title} />
@@ -46,6 +79,7 @@ const BookList: React.FC = () => {
           </div>
         ))}
       </div>
+      {loading && <p>Loading more books...</p>}
     </div>
   );
 };
